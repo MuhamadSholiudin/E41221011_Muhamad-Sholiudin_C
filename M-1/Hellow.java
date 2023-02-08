3e20754353d0ce6fdb01e88e9eed2504288f584b
E41221011_Muhamad Sholiudin_C <124753912+MuhamadSholiudin@users.noreply.github.com>
2023-02-08 17:00:53 +0700

    Create Hellow.java

---
+++ M-1/Hellow.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellow;
+
+/**
+ *
+ * @author SVC
+ */
+public class Hellow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hai guys!! gw lagi belajar nginstall GitHub nichhh!!");
+    }
+    
+}
